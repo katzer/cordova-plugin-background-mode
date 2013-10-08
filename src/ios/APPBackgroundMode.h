8cc9be6262df86ca0824b66e4fb38a4ee820830d
Sebastián Katzer <katzer.sebastian@googlemail.com>
2013-10-08 11:00:36 +0200

    Start with iOS support

---
+++ src/ios/APPBackgroundMode.h
@@ -0,0 +1,20 @@
+/**
+ *  APPBackgroundMode.h
+ *  Cordova BackgroundMode Plugin
+ *
+ *  Created by Sebastian Katzer (github.com/katzer) on 08/10/2013.
+ *  Copyright 2013 Sebastian Katzer. All rights reserved.
+ *  GPL v2 licensed
+ */
+
+#import <Foundation/Foundation.h>
+#import <Cordova/CDVPlugin.h>
+
+@interface APPBackgroundMode : CDVPlugin
+
+// Aktiviert den Hintergrundmodus
+- (void) activateMode;
+// Deaktiviert den Hintergrundmodus
+- (void) deactivateMode;
+
+@end
---
+++ src/ios/APPBackgroundMode.m
@@ -0,0 +1,26 @@
+/**
+ *  APPBackgroundMode.m
+ *  Cordova BackgroundMode Plugin
+ *
+ *  Created by Sebastian Katzer (github.com/katzer) on 08/10/2013.
+ *  Copyright 2013 Sebastian Katzer. All rights reserved.
+ *  GPL v2 licensed
+ */
+
+#import "APPBackgroundMode.h"
+
+@implementation APPBackgroundMode
+
+// Aktiviert den Hintergrundmodus
+- (void) activateMode
+{
+
+}
+
+// Deaktiviert den Hintergrundmodus
+- (void) deactivateMode
+{
+
+}
+
+@end
