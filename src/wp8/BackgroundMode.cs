433e216439cc3a6b7650e679395dc65ae19cbd94
Sebastián Katzer <katzer.sebastian@googlemail.com>
2013-10-08 17:59:20 +0200

    ...

---
+++ src/wp8/BackgroundMode.cs
@@ -0,0 +1,27 @@
+/**
+ *  BackgroundMode.cs
+ *  Cordova Background-Mode Plugin
+ *
+ *  Created by Sebastian Katzer (github.com/katzer) on 08/10/2013.
+ *  Copyright 2013 Sebastian Katzer. All rights reserved.
+ *  GPL v2 licensed
+ */
+
+using System;
+using System.Linq;
+
+using Microsoft.Phone.Shell;
+
+using WPCordovaClassLib.Cordova;
+using WPCordovaClassLib.Cordova.Commands;
+
+namespace APPPlant.Cordova.Plugin
+{
+    public class BackgroundMode : BaseCommand
+    {
+        public BackgroundMode ()
+        {
+
+        }
+    }
+}
