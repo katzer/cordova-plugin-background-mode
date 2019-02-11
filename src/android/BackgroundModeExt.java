5f6072e738e2d171d657715162ab58d9bd15ab9c
Sebastián Katzer <katzer.sebastian@googlemail.com>
2019-02-11 15:23:27 +0100

    Fix type in clearScreenAndKeyguardFlags

--- src/android/BackgroundModeExt.java
+++ src/android/BackgroundModeExt.java
@@ -1,22 +1,22 @@
 /*
- Copyright 2013 Sebastián Katzer
+    Copyright 2013-2017 appPlant GmbH
 
- Licensed to the Apache Software Foundation (ASF) under one
- or more contributor license agreements.  See the NOTICE file
- distributed with this work for additional information
- regarding copyright ownership.  The ASF licenses this file
- to you under the Apache License, Version 2.0 (the
- "License"); you may not use this file except in compliance
- with the License.  You may obtain a copy of the License at
+    Licensed to the Apache Software Foundation (ASF) under one
+    or more contributor license agreements.  See the NOTICE file
+    distributed with this work for additional information
+    regarding copyright ownership.  The ASF licenses this file
+    to you under the Apache License, Version 2.0 (the
+    "License"); you may not use this file except in compliance
+    with the License.  You may obtain a copy of the License at
 
- http://www.apache.org/licenses/LICENSE-2.0
+     http://www.apache.org/licenses/LICENSE-2.0
 
- Unless required by applicable law or agreed to in writing,
- software distributed under the License is distributed on an
- "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- KIND, either express or implied.  See the License for the
- specific language governing permissions and limitations
- under the License.
+    Unless required by applicable law or agreed to in writing,
+    software distributed under the License is distributed on an
+    "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+    KIND, either express or implied.  See the License for the
+    specific language governing permissions and limitations
+    under the License.
  */
 
 package de.appplant.cordova.plugin.background;
@@ -176,7 +176,7 @@ public class BackgroundModeExt extends CordovaPlugin {
                 Intent.FLAG_ACTIVITY_SINGLE_TOP |
                 Intent.FLAG_ACTIVITY_CLEAR_TOP);
 
-        clearSreenAndKeyguardFlags();
+        clearScreenAndKeyguardFlags();
         app.startActivity(intent);
     }
 
@@ -397,7 +397,7 @@ public class BackgroundModeExt extends CordovaPlugin {
     /**
      * Clears required flags to the window to unlock/wakeup the device.
      */
-    private void clearSreenAndKeyguardFlags()
+    private void clearScreenAndKeyguardFlags()
     {
         getApp().runOnUiThread(() -> getApp().getWindow().clearFlags(FLAG_ALLOW_LOCK_WHILE_SCREEN_ON | FLAG_SHOW_WHEN_LOCKED | FLAG_TURN_SCREEN_ON | FLAG_DISMISS_KEYGUARD));
     }
