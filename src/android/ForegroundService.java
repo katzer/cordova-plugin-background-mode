40315e4c56682ca7f953aa5056dcf6002ea88ba9
valeckonext <mvalecko@nextivityinc.com>
2023-03-22 10:29:28 -0700

    add notification fix for android SDK 31

--- src/android/ForegroundService.java
+++ src/android/ForegroundService.java
@@ -23,10 +23,7 @@ package de.appplant.cordova.plugin.background;
 
 import android.annotation.SuppressLint;
 import android.annotation.TargetApi;
-import android.app.Notification;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.app.Service;
+import android.app.*;
 import android.content.Context;
 import android.content.Intent;
 import android.content.res.Resources;
@@ -34,8 +31,6 @@ import android.os.Binder;
 import android.os.Build;
 import android.os.IBinder;
 import android.os.PowerManager;
-import android.app.NotificationChannel;
-
 import org.json.JSONObject;
 
 import static android.os.PowerManager.PARTIAL_WAKE_LOCK;
@@ -219,10 +214,16 @@ public class ForegroundService extends Service {
         setColor(notification, settings);
 
         if (intent != null && settings.optBoolean("resume")) {
+            int flags = PendingIntent.FLAG_UPDATE_CURRENT;
+
+            if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                flags = flags | PendingIntent.FLAG_MUTABLE;
+            }
+
             intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
             PendingIntent contentIntent = PendingIntent.getActivity(
-                    context, NOTIFICATION_ID, intent,
-                    PendingIntent.FLAG_UPDATE_CURRENT);
+                context, NOTIFICATION_ID, intent,
+                flags);
 
 
             notification.setContentIntent(contentIntent);
